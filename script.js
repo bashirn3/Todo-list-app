12116376ffec7c21efb1c2dd2d7baf85cc85a116
Bashir <@bashirrn3@gmail.com>
2020-01-26 21:32:45 +0100

    made changes to script.js

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 var button = document.querySelector("button");
 var list = document.querySelector("ul");
 var input = document.querySelector("input");
+var clr = document.getElementById("clear")
 
+clear.addEventListener("click" , function() {
+   document.querySelector('ul').innerHTML="";
+})
 
 button.addEventListener("click" , function(){
   
 var todo = input.value;
 
-var text = document.createTextNode(todo);
 var newItem = document.createElement('li');
+var del = document.createElement("button");
+var space = document.createElement("span");
 
+var text = document.createTextNode(todo);
+var btnText = document.createTextNode("Delete");
+space.textContent ='  ';
 
+del.appendChild(btnText);
 
 newItem.appendChild(text);
+newItem.appendChild(space);
+newItem.appendChild(del);
+
+
 list.appendChild(newItem);
+
+del.addEventListener("click" , function(){
    
+   list.removeChild(newItem);
+
+  })   
 
 
 })
