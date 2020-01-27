5106e154c9d349256510ede0ac67def53ed8f129
Bashir <@bashirrn3@gmail.com>
2020-01-27 17:46:12 +0100

    made changes to the js file

--- script.js
+++ script.js
@@ -7,26 +7,29 @@ clear.addEventListener("click" , function() {
    document.querySelector('ul').innerHTML="";
 })
 
+
+
 button.addEventListener("click" , function(){
   
 var todo = input.value;
+if (todo === ''){
+  alert("Please insert a todo")
+}
+
+else {
 
 var newItem = document.createElement('li');
 var del = document.createElement("button");
-var space = document.createElement("span");
+del.innerHTML ="Delete";
 
-var text = document.createTextNode(todo);
-var btnText = document.createTextNode("Delete");
-space.textContent ='  ';
 
-del.appendChild(btnText);
 
-newItem.appendChild(text);
-newItem.appendChild(space);
+newItem.innerHTML= todo + " ";
 newItem.appendChild(del);
-
-
 list.appendChild(newItem);
+
+}
+
 
 del.addEventListener("click" , function(){
    
